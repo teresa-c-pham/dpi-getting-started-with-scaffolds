bc1b1bb15c5212edc865d0b82084957c541d7c44
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-12-17 04:52:34 +0000

    implement create and visit movies

---
+++ app/models/movie.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  released    :boolean
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+end
